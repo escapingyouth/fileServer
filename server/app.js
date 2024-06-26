7169d06f4f311ba063f54cc1f842005429f41c28
Michael Appiah <mke.appiah@gmail.com>
2024-06-26 18:37:07 +0000

    Add more dev data

--- server/app.js
+++ server/app.js
@@ -13,10 +13,9 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-// app.use(multer().single('uploadFile'));
 
 app.use('/api/files', fileRouter);
 
